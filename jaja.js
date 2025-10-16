ec5bbbdb9f4290b1ad6d0a1bf6341f9358ea1246
hannalong <hannalong@gmail.com>
2025-10-16 09:05:30 +0700

    comment

---
+++ jaja.js
@@ -0,0 +1,17 @@
+//var nume = "top";
+//console.log(nume);
+
+//var birth = "31/10/2003";
+//console.log(birth);
+
+var nume = "top", lastname = "savayvanh", adderss = "phonthan", birth = "31/10/2003";
+console.log( nume,lastname,adderss,birth);
+
+var x = 10, y = 30, o = 80;
+console.log("ກ່ອນ", x);
+//x+=y;
+//x=x-y-o;
+//x*=y;
+x=x/y/o;
+console.log("ຫຼັງ", x);
+
